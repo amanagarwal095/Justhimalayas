d3c8175fe166da0b0fffb2f3e2ce033312ffdd04
amanagarwal095 <104392258+amanagarwal095@users.noreply.github.com>
2023-01-17 00:15:44 +0530

    website

---
+++ index.js
@@ -0,0 +1,26 @@
+// Smooth Scrolling
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+  
+  // Show/hide navigation on scroll
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
